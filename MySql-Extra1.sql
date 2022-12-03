0075980ee77d4f70e396c745da2f9c4f4e80282e
Quynh Pham <quynhphamalone2@gmail.com>
2022-12-04 03:42:25 +0900

    first commit

--- MySql-Extra1.sql
+++ MySql-Extra1.sql
@@ -7,13 +7,13 @@ PRIMARY KEY (TraineeID),
 Full_Name varchar(100) NOT NULL,
 Birth_Date DATE,
 Gender Enum ('male', 'female', 'unknown'),
-ET_IQ int,
+ET_IQ int
 	CHECK (ET_IQ > 0),
 	CHECK (ET_IQ < 20),
-ET_Gmath int,
+ET_Gmath int
 	CHECK (ET_Gmath > 0),
 	CHECK (ET_Gmath < 20),
-ET_English int,
+ET_English int
 	CHECK (ET_English > 0),
 	CHECK (ET_English < 50),
 Training_Class varchar(255),
